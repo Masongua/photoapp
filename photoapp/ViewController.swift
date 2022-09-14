23f6de9555494192b231f399cf34eceb05afc056
Mason Guaman <masonstalin@gmail.com>
2022-09-14 14:18:32 -0400

    rtht

--- photoapp/ViewController.swift
+++ photoapp/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var reetlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func button(_ sender: Any) {
+    }
+    
 }
 
